478c326b9a6fa6e38f39c122ca593876f29f6a20
Tom COLEGNO <tomcolegno@Toms-Laptop.local>
2024-05-14 09:56:39 +0200

    adding code for index.js

---
+++ jour02/index.js
@@ -0,0 +1,9 @@
+// index.js
+
+const app = require('./server'); // Import du serveur Express depuis server.js
+
+// Démarrage du serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
