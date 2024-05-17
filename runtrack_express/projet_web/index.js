6de8a75272a58ba191fd8dcbd454eb0be25a1162
Tom COLEGNO <tomcolegno@Toms-Laptop.local>
2024-05-17 10:41:51 +0200

    day 5 job 1 done

---
+++ runtrack_express/projet_web/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app/*get*/('/', (req, res) => {
+  res.send('Bonjour, ceci est votre serveur web!');
+});
+
+app.listen(port, () => {
+  console.log(`Le serveur est en écoute sur le port ${port}`);
+});
