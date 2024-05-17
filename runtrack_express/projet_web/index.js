9d9902ff8ac0b08c1585f18e8422bee86535101c
Tom COLEGNO <tomcolegno@Toms-Laptop.local>
2024-05-17 10:45:23 +0200

    day5 job4 done

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.send('Bonjour, ceci est votre serveur web!');
+  res.send('<h1>Page d\'accueil</h1><p>Ceci est la page d\'accueil de mon site web.</p>');
+});
+
+// Route pour la page "about"
+app.get('/about', (req, res) => {
+  res.send('<h1>À propos</h1><p>Cette page présente mon projet.</p>');
 });
 
 app.listen(port, () => {
